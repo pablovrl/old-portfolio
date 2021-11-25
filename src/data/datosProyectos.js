4c06c2a296cffc3a25b3a0fbe8f93e91d60d35b6
Pablo Villarroel <pablovillarroel135@gmail.com>
2021-11-25 16:25:14 -0300

    se creó un nuevo archivo para guardar los datos de los proyectos

---
+++ src/data/datosProyectos.js
@@ -0,0 +1,22 @@
+const proyectos = [
+  {
+    nombre: "To-Do App",
+    descripcion: "Una simple aplicación de TODO's en React, conectada a una API, y con requests realizadas mediante axios.",
+    link: "https://react-todo-app-aanrkme15-pablovrl.vercel.app/",
+    github: "https://github.com/pabloVrl/react-todo-app"
+  },
+  {
+    nombre: "API To-Do App",
+    descripcion: "API REST para la aplicación de To-Do's, desarrollada con Express, y usando MongoDB para la continuidad de los datos.",
+    link: "",
+    github: "https://github.com/pabloVrl/api-todo-app"
+  },
+  {
+    nombre: "Portafolio",
+    descripcion: "El portfolio que estás viendo actualmente, desarrollado con React y Chakra UI.",
+    link: "",
+    github: "https://github.com/pabloVrl/portfolio"
+  }
+]
+
+export default proyectos
--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -5,30 +5,9 @@ import {
 } from '@chakra-ui/react'
 import Proyecto from '../components/Proyecto'
 import TextoConIcono from '../components/TextoConIcono'
-
+import proyectos from '../data/datosProyectos'
 
 export default function Proyectos() {
-
-  const proyectos = [
-    {
-      nombre: "To-Do App",
-      descripcion: "Una simple aplicación de TODO's en React, conectada a una API, y con requests realizadas mediante axios.",
-      link: "https://react-todo-app-aanrkme15-pablovrl.vercel.app/",
-      github: "https://github.com/pabloVrl/react-todo-app"
-    },
-    {
-      nombre: "API To-Do App",
-      descripcion: "API REST para la aplicación de To-Do's, desarrollada con Express, y usando MongoDB para la continuidad de los datos.",
-      link: "",
-      github: "https://github.com/pabloVrl/api-todo-app"
-    },
-    {
-      nombre: "Portafolio",
-      descripcion: "El portfolio que estás viendo actualmente, desarrollado con React y Chakra UI.",
-      link: "",
-      github: "https://github.com/pabloVrl/portfolio"
-    }
-  ]
 
   const componentesProyectos = proyectos.map( p => <Proyecto proyecto={p} />)
 
